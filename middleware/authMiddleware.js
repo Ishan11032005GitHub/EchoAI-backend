cc4bb287531e75f66be82f3fcdb8b91cc1c45884
Ishan11032005GitHub <Ishan11032005@gmail.com>
2025-07-15 10:55:00 +0530

    Add

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,9 +2,10 @@ import jwt from 'jsonwebtoken';
 
 const authMiddleware = (req, res, next) => {
   const authHeader = req.headers.authorization;
-  if (!authHeader) return res.status(401).json({ message: 'No token provided' });
+  const token = authHeader?.startsWith('Bearer ') 
+  ? authHeader.split(' ')[1]
+  : req.cookies?.token;
 
-  const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.userId = decoded.userId;
