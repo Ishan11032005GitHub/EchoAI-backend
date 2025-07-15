8dc7622094f24b63a986384fdc096d43a005683a
Ishan11032005GitHub <Ishan11032005@gmail.com>
2025-07-15 10:33:05 +0530

    Add

--- server.js
+++ server.js
@@ -57,7 +57,9 @@ app.use(cors({
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
 }));
+app.options('*', cors());
 
 app.use(express.json());
 app.use(cookieParser());
